bf0f81363ec89a8b9a015b883028c40c050c8025
Hasibur Rahman <hasib.active@gmail.com>
2022-05-07 23:23:13 +0600

    update auth errors

--- app/Controllers/Admin/AdminAuthController.ts
+++ app/Controllers/Admin/AdminAuthController.ts
@@ -2,11 +2,14 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { AdminLoginValidator } from 'App/Validators/AuthValidator'
 
 export default class AuthController {
-  public async login({ auth, request }: HttpContextContract) {
+  public async login({ auth, request, response }: HttpContextContract) {
     const payload = await request.validate(AdminLoginValidator)
-    const token = await auth.use('admin').attempt(payload.email, payload.password)
-
-    return token
+    try {
+      const token = await auth.use('admin').attempt(payload.email, payload.password)
+      return token
+    } catch (error) {
+      return response.status(400).json({ errors: [{ message: 'Email or Password is wrong!' }] })
+    }
   }
 
   public me({ auth }: HttpContextContract) {
--- app/Controllers/User/UserAuthController.ts
+++ app/Controllers/User/UserAuthController.ts
@@ -3,11 +3,15 @@ import User from 'App/Models/User'
 import { UserLoginValidator, UserRegisterValidator } from 'App/Validators/AuthValidator'
 
 export default class AuthController {
-  public async login({ auth, request }: HttpContextContract) {
+  public async login({ auth, request, response }: HttpContextContract) {
     const payload = await request.validate(UserLoginValidator)
-    const response = await auth.use('user').attempt(payload.email, payload.password)
 
-    return response
+    try {
+      const response = await auth.use('user').attempt(payload.email, payload.password)
+      return response
+    } catch (error) {
+      return response.status(400).json({ errors: [{ message: 'Email or Password is wrong!' }] })
+    }
   }
 
   public async register({ request, auth }: HttpContextContract) {
